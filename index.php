9cf04d9683ac7bdd24f51b2e1530963035019ae3
Richard Wilkes <rich@trollworks.com>
2014-05-29 18:47:40 -0700

    Push out the 4.0 release.

--- index.php
+++ index.php
@@ -17,12 +17,15 @@
 				<div class="box">
 					<img id="logo" src="images/logo.gif" title="GURPS Character Sheet" />
 					<div id="downloads">
-						<h2>Download<br>Version 2011.01.02.1054</h2>
-						<p><a href="http://sourceforge.net/projects/gcs-java/files/GCS-2011.01.02.1054.zip/download" id="macintosh_download" title="Click to download the Macintosh version"></a>
-							<a href="http://sourceforge.net/projects/gcs-java/files/GCS-2011.01.02.1054.zip/download" id="windows_download" title="Click to download the Windows version"></a>
-							<a href="http://sourceforge.net/projects/gcs-java/files/GCS-2011.01.02.1054.zip/download" id="linux_download" title="Click to download the Linux version"></a>
-							<br>Released January 2, 2011</p>
+						<h2>Download<br>Version 4.0</h2>
+						<p><a href="http://sourceforge.net/projects/gcs-java/files/gcs-4.0.0-mac.zip/download" id="macintosh_download" title="Click to download the Macintosh version"></a>
+							<a href="http://sourceforge.net/projects/gcs-java/files/gcs-4.0.0-windows.zip/download" id="windows_download" title="Click to download the Windows version"></a>
+							<a href="http://sourceforge.net/projects/gcs-java/files/gcs-4.0.0-linux.zip/download" id="linux_download" title="Click to download the Linux version"></a>
+							<br>Released May 29, 2014</p>
 					</div>
+					<p>I am looking for volunteers to review the data files, find any mistakes or missing bits, make the necessary
+						updates, and send them to me for inclusion in the next update. If you'd like to help out,
+						<a href="mailto:rich@trollworks.com">contact me</a>.</p> 
 				</div>
 			</div>
 		</div>
--- release_notes.php
+++ release_notes.php
@@ -11,6 +11,43 @@
 			<div id="right_column">
 				<div id="release_notes">
 					<h1>Release Notes</h1>
+					<h2>Version <span class="version">4.0</span><span class="release_date">May 29, 2014</span></h2>
+					<h3>Features Added</h3>
+					<ul>
+						<li>Added native launchers for each platform and embedded an appropriate Java Runtime Environment
+							so that GCS will always have the version it needs to run.</li>
+						<li>Implemented a docking interface for all of the files. This replaces the old window-based
+							file management.</li>
+						<li>Replaced the Data menu with the Library Explorer.</li>
+						<li>Switched back to a version numbering scheme more familiar to most people, i.e.
+							&lt;major&gt;.&lt;minor&gt;.&lt;bugfix&gt;. This has the unfortunate side-effect of not
+							allowing older versions to notice there is a new version available for this transition.</li>
+					</ul>
+					<h3>Bugs Fixed</h3>
+					<ul>
+						<li>Fix for <a href="http://sourceforge.net/p/gcs-java/bugs/80">Bug #80</a>: Issues with localized
+							numbers not being saved and restored correctly when the system locale wasn&#039;t compatible
+							with US settings.</li>
+						<li>Fix for <a href="http://sourceforge.net/p/gcs-java/bugs/78">Bug #78</a>: Equipment prereq
+							doesn&#039;t store weight values &gt;= 1000.</li>
+						<li>Fix for <a href="http://sourceforge.net/p/gcs-java/bugs/79">Bug #79</a>: Optional
+							multiplicative modifiers aren&#039;t calculated correctly.</li>
+					</ul>
+					<h3>Miscellaneous Changes</h3>
+					<ul>
+						<li>Updated the code base to require Java 8.</li>
+					</ul>
+					<h3>Data File Changes</h3>
+					<ul>
+						<li>As part of the change to use a docking interface, split the library files (.glb) back out
+							into their separate library parts: Advantages (.adq), Skills (.skl), Spells (.spl), and
+							Equipment (.eqp). You can still load the old .glb files, but untitled library documents of
+							the appropriate type will be created when you do so, allowing you to save them out
+							individually.</li>
+						<li>Added (anti)talents from Power Ups 3, including reaction bonuses and alternate benefits.
+							Contributed by Kevin Michael Smith.</li>
+						<li>Updated the Action library with the latest from Zachary Spector.</li>
+					</ul>
 					<h2>Version <span class="version">2011.01.02.1054</span><span class="release_date">January 2, 2011</span></h2>
 					<h3>Features Added</h3>
 					<ul>
