d00e461cc197346a78c5d8037f35446874017e9f
Richard Wilkes <richard.wilkes@arm.com>
2015-01-07 14:59:24 -0800

    Added license link to footer. Updated copyright year to include 2015.

--- parts/footer.php
+++ parts/footer.php
@@ -1,4 +1,4 @@
 		<div id="footer">
-			Copyright &copy;1999-2014 by Richard A. Wilkes. All rights reserved worldwide.<br>
-			GURPS Character Sheet is licensed under the Mozilla Public License, version 2.0.
+			Copyright &copy;1999-2015 by Richard A. Wilkes. All rights reserved worldwide.<br>
+			GURPS Character Sheet is licensed under the <a href="/license.html">Mozilla Public License, version 2.0</a>.
 		</div>
