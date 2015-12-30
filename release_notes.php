b543306e6a2163d703143f14091a5767bf6bf027
Richard Wilkes <rich@trollworks.com>
2015-12-29 16:07:25 -0800

    Push GCS 4.2

--- release_notes.php
+++ release_notes.php
@@ -11,6 +11,32 @@
 			<div id="right_column">
 				<div id="release_notes">
 					<h1>Release Notes</h1>
+					<h2>Version <span class="version">4.2</span><span class="release_date">December 29, 2015</span></h2>
+					<h3>Features Added</h3>
+					<ul>
+						<li>Implemented <a href="https://gurpscharactersheet.atlassian.net/browse/GCS-15">GCS-15</a>: Add option to display skill default used.</li>
+						<li>Implemented <a href="https://gurpscharactersheet.atlassian.net/browse/GCS-50">GCS-50</a>: Support manual switching of defaults as per B173. Contributed by Sebástien Wilhelmi.</li>
+					</ul>
+					<h3>Bugs Fixed</h3>
+					<ul>
+						<li>Drag-rearrangement of tabs on Linux was not working.</li>
+						<li>Fix for <a href="https://gurpscharactersheet.atlassian.net/browse/GCS-51">GCS-51</a>: Skill editor drops skill points to 0 when a language that uses '.' for a numeric grouping separator is used.</li>
+						<li>Fix for <a href="https://gurpscharactersheet.atlassian.net/browse/GCS-29">GCS-29</a>: Too much precision in randomized height & weight.</li>
+						<li>Updated the URLs for filing bug reports and feature requests to point to the new Jira server I'm using.</li>
+					</ul>
+					<h3>Data File Changes</h3>
+					<ul>
+						<li>Added data files for Reign of Steel: Will to Live. Contributed by Иван Абдулин.</li
+						<li>Added <a href="https://gurpscharactersheet.atlassian.net/browse/GCS-41">GCS-41</a>: GURPS Instant Armor. Contributed by William Morris.</li>
+						<li>Fix for <a href="https://gurpscharactersheet.atlassian.net/browse/GCS-43">GCS-43</a>: Decreased Basic Speed missing from Basic Advantages. Contributed by William Morris.</li>
+						<li>Fix for <a href="https://gurpscharactersheet.atlassian.net/browse/GCS-42">GCS-42</a>: Weapon Bond missing from Dungeon Fantasy Advantages. Contributed by William Morris.</li>
+						<li>Added equipment from Low Tech. Contributed by William Morris.</li>
+						<li>Fix for <a href="https://gurpscharactersheet.atlassian.net/browse/GCS-30">GCS-30</a>: Dungeon fantasy containers don't have the correct weight limits for what they can hold.
+							Additional fixes were made for various other incorrect entries as well. Contributed by William Morris.</li>
+						<li>Added advantages and spells from GURPS Magic - Plant Spells. Contributed by Klara.</li>
+						<li>Added a template to simulate the background of having gone through the German public school system. Contributed by Christian.</li>
+						<li>Added a compilation of Low Tech equipment. Contributed by James B. Byrne.</li>
+					</ul>
 					<h2>Version <span class="version">4.1.1</span><span class="release_date">July 4, 2015</span></h2>
 					<h3>Bugs Fixed</h3>
 					<ul>
