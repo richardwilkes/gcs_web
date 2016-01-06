698bc1d65d63441d3812ac6e63d07cabc0cd6294
Richard Wilkes <rich@trollworks.com>
2016-01-06 11:26:39 -0800

    Fix copyright start.

--- parts/footer.php
+++ parts/footer.php
@@ -1,4 +1,4 @@
 		<div id="footer">
-			Copyright &copy;1999-2016 by Richard A. Wilkes. All rights reserved worldwide.<br>
+			Copyright &copy;1998-2016 by Richard A. Wilkes. All rights reserved worldwide.<br>
 			GURPS Character Sheet is licensed under the <a href="/license.html">Mozilla Public License, version 2.0</a>.
 		</div>
