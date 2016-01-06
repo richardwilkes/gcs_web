349cabe14a289762d0d62a3d60e3b21309b16066
Richard Wilkes <rich@trollworks.com>
2016-01-06 12:15:06 -0800

    Added some more links.

--- parts/menu.php
+++ parts/menu.php
@@ -2,6 +2,7 @@
 				<div id="menu">
 					<ul>
 						<li><a href="/index.php">Home</a></li>
+						<li><a href="/releases.php">Releases</a></li>
 						<li><a href="/release_notes.php">Release Notes</a></li>
 						<li><a href="/in_progress_notes.php">&nbsp;&nbsp;... In Progress Notes</a></li>
 						<li><a href="/faq.php">FAQ</a></li>
@@ -11,7 +12,6 @@
 						<li><a href="https://gurpscharactersheet.atlassian.net/issues/?filter=10000">Feature Requests</a></li>
 						<li><a href="/development.php">Development Setup</a></li>
 						<li><a href="/output_templates.php">Output Templates</a></li>
-						<li><a href="/releases.php">Releases</a></li>
 						<li><a href="mailto:rich@trollworks.com">Contact Me</a></li>
 						<li><a href="http://sourceforge.net/project/project_donations.php?group_id=185516">Donate</a></li>
 					</ul>
@@ -21,6 +21,8 @@
 					<ul>
 						<li><a href="http://sjgames.com">Steve Jackson Games</a></li>
 						<li><a href="http://www.sjgames.com/gurps/abbrevs.html">GURPS Abbreviations</a></li>
+						<li><a href="http://gamingballistic.blogspot.com">Gaming Ballistic</a><li>
+						<li><a href="http://dungeonfantastic.blogspot.com">Dungeon Fantastic</a></li>
 						<li><a href="http://www.gamesdiner.com/gurps-diner">GURPS Diner</a></li>
 						<li><a href="http://www.thecabal.org/gurps/rareitems">Rare GURPS Items</a></li>
 						<li><a href="http://greywolf.critter.net/fonts.htm">Greywolf Fonts</a></li>
