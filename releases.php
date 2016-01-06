5971a173c61f7d233183f46f0d325362dfde329f
Richard Wilkes <rich@trollworks.com>
2016-01-06 12:04:27 -0800

    Created a 'Releases' page.

---
+++ releases.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>GURPS Character Sheet - Releases</title>
+		<?php include 'parts/head.php'; ?>
+	</head>
+	<body>
+		<?php include 'parts/page_header.php'; ?>
+		<div id="main">
+			<?php include 'parts/menu.php'; ?>
+			<div id="right_column">
+				<h1>Releases</h1>
+				<h2>Version <span class="version">4.2.0</span><span class="release_date">December 29, 2015</span></h2>
+				<p></p>
+				<ul>
+					<li><a href="/downloads/4.2.0/gcs-4.2.0-mac.zip" id="macintosh_download" title="Click to download the Macintosh version"></a></li>
+					<li><a href="/downloads/4.2.0/gcs-4.2.0-windows.zip" id="windows_download" title="Click to download the 64-bit Windows version"></a></li>
+					<li><a href="/downloads/4.2.0/gcs-4.2.0-windows-32.zip" id="windows32_download" title="Click to download the 32-bit Windows version"></a></li>
+					<li><a href="/downloads/4.2.0/gcs-4.2.0-linux.zip" id="linux_download" title="Click to download the 64-bit Linux version"></a></li>
+					<li><a href="/downloads/4.2.0/gcs-4.2.0-linux-32.zip" id="linux32_download" title="Click to download the 32-bit Linux version"></a></li>
+				</ul>
+				<h2>Version <span class="version">4.1.1</span><span class="release_date">July 4, 2015</span></h2>
+				<p></p>
+				<ul>
+					<li><a href="/downloads/4.1.1/gcs-4.1.1-mac.zip" id="macintosh_download" title="Click to download the Macintosh version"></a></li>
+					<li><a href="/downloads/4.1.1/gcs-4.1.1-windows.zip" id="windows_download" title="Click to download the 64-bit Windows version"></a></li>
+					<li><a href="/downloads/4.1.1/gcs-4.1.1-windows-32.zip" id="windows32_download" title="Click to download the 32-bit Windows version"></a></li>
+					<li><a href="/downloads/4.1.1/gcs-4.1.1-linux.zip" id="linux_download" title="Click to download the 64-bit Linux version"></a></li>
+					<li><a href="/downloads/4.1.1/gcs-4.1.1-linux-32.zip" id="linux32_download" title="Click to download the 32-bit Linux version"></a></li>
+				</ul>
+				<h2>Version <span class="version">4.1.0</span><span class="release_date">April 19, 2015</span></h2>
+				<p></p>
+				<ul>
+					<li><a href="/downloads/4.1.0/gcs-4.1.0-mac.zip" id="macintosh_download" title="Click to download the Macintosh version"></a></li>
+					<li><a href="/downloads/4.1.0/gcs-4.1.0-windows.zip" id="windows_download" title="Click to download the 64-bit Windows version"></a></li>
+					<li><a href="/downloads/4.1.0/gcs-4.1.0-windows-32.zip" id="windows32_download" title="Click to download the 32-bit Windows version"></a></li>
+					<li><a href="/downloads/4.1.0/gcs-4.1.0-linux.zip" id="linux_download" title="Click to download the 64-bit Linux version"></a></li>
+					<li><a href="/downloads/4.1.0/gcs-4.1.0-linux-32.zip" id="linux32_download" title="Click to download the 32-bit Linux version"></a></li>
+				</ul>
+				<h1>Ancient Releases</h2>
+				<p>For those of you with very old Macs, I've kept around some ancient versions of GCS that you can try.
+					These versions were written for the <a href="http://www.sjgames.com/gurps">GURPS 3rd Edition</a>
+					roleplaying game system.</p>
+				<p></p>
+				<h2>Version <span class="version">2.2.3</span><span class="release_date">August 27, 2002</span></h2>
+				<p></p>
+				<ul>
+					<li><a href="/downloads/ancient/gcs-2.2.3.sit">Version 2.2.3</a>: Supports PowerPC processors running Mac OS 8.5+</li>
+				</ul>
+				<h2>Version <span class="version">1.0.5</span><span class="release_date">February 16, 1999</span></h2>
+				<p></p>
+				<ul>
+					<li><a href="/downloads/ancient/gcs-1.0.5.sit">Version 1.0.5</a>: Supports 68K and PowerPC processors</li>
+				</ul>
+			</div>
+		</div>
+		<?php include 'parts/footer.php'; ?>
+	</body>
+</html>
