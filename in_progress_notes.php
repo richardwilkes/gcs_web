c259581d854916549843632a004e2319e30b74a7
Richard Wilkes <wilkes@me.com>
2016-07-12 09:35:38 -0700

    Note change for GCS_LIBRARY override.

--- in_progress_notes.php
+++ in_progress_notes.php
@@ -14,7 +14,9 @@
 					<h2>Version <span class="version">&lt;in development&gt;</span><span class="release_date">Not released yet</span></h2>
 					<h3>Features Added</h3>
 					<ul>
-						<li></li>
+						<li>By default, GCS looks for its library of data files next to itself in its installation directory.
+							You can now alter this behavior by setting the environment variable GCS_LIBRARY to point to some
+							other location on disk instead.</li>
 					</ul>
 					<h3>Bugs Fixed</h3>
 					<ul>
