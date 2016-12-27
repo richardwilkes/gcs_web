b625f62aec62f4a1c68657500661e5754e42ab4f
Richard Wilkes <wilkes@me.com>
2016-12-27 08:53:22 -0800

    Make Jira link take you to a reasonable location

--- parts/menu.php
+++ parts/menu.php
@@ -8,7 +8,7 @@
 						<li><a href="/faq.php">FAQ</a></li>
 						<li><a href="/mailing_lists.php">Mailing Lists</a></li>
 						<li><a href="http://sourceforge.net/projects/gcs-java/reviews">Reviews</a></li>
-						<li><a href="https://gurpscharactersheet.atlassian.net">Jira</a></li>
+						<li><a href="https://gurpscharactersheet.atlassian.net/projects/GCS">Jira</a></li>
 						<li><a href="https://github.com/richardwilkes/gcs/blob/master/README.md">Development Setup</a></li>
 						<li><a href="/output_templates.php">Output Templates</a></li>
 						<li><a href="https://drive.google.com/drive/folders/0BwDjN2HwBhv4V1NJVWJtWUNWcUU?usp=sharing">User Content</a></li>
