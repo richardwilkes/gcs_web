52f5fade9b877bdec1bef3d6f993d95ffdc2034f
Richard Wilkes <wilkes@me.com>
2016-12-29 12:41:24 -0800

    Initial checkin

---
+++ refs.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>GURPS Character Sheet - Page References</title>
+		<?php include 'parts/head.php'; ?>
+	</head>
+	<body>
+		<?php include 'parts/page_header.php'; ?>
+		<div id="main">
+			<?php include 'parts/menu.php'; ?>
+			<div id="right_column">
+				<h1>Page References</h1>
+				<h3>These are the page reference abbreviations being used in the data files:</h3>
+				<p><table>
+					<tr><td><b>ACT1:</b></td><td>Action 1: Heroes</td></tr>
+					<tr><td><b>ACT3:</b></td><td>Action 3: Furious Fists</td></tr>
+					<tr><td><b>B</b></td><td>Basic Set</td></tr>
+					<tr><td><b>BS</b></td><td>Banestorm</td></tr>
+					<tr><td><b>DF1:</b></td><td>Dungeon Fantasy 1: Adventurers</td></tr>
+					<tr><td><b>DF3:</b></td><td>Dungeon Fantasy 3: The Next Level</td></tr>
+					<tr><td><b>DF4:</b></td><td>Dungeon Fantasy 4: Sages</td></tr>
+					<tr><td><b>DF8:</b></td><td>Dungeon Fantasy 8: Treasure Tables</td></tr>
+					<tr><td><b>DF11:</b></td><td>Dungeon Fantasy 11: Power-Ups</td></tr>
+					<tr><td><b>DF12:</b></td><td>Dungeon Fantasy 12: Ninja</td></tr>
+					<tr><td><b>DF13:</b></td><td>Dungeon Fantasy 13: Loadouts</td></tr>
+					<tr><td><b>DF14:</b></td><td>Dungeon Fantasy 14: Psi</td></tr>
+					<tr><td><b>DFM1:</b></td><td>Dungeon Fantasy Monsters 1</td></tr>
+					<tr><td><b>DR</b></td><td>Dragons</td></tr>
+					<tr><td><b>F</b></td><td>Fantasy</td></tr>
+					<tr><td><b>GUL</b></td><td>Gulliver Mini</td></tr>
+					<tr><td><b>H</b></td><td>Horror</td></tr>
+					<tr><td><b>HF</b></td><td>Historical Folks</td></tr>
+					<tr><td><b>HT</b></td><td>High-Tech</td></tr>
+					<tr><td><b>IW</b></td><td>Infinite Worlds</td></tr>
+					<tr><td><b>LT</b></td><td>Low-Tech</td></tr>
+					<tr><td><b>LTC1:</b></td><td>Low-Tech Companion 1: Philosophers and Kings</td></tr>
+					<tr><td><b>LTIA</b></td><td>Low-Tech: Instant Armor</td></tr>
+					<tr><td><b>M</b></td><td>Magic</td></tr>
+					<tr><td><b>MPS</b></td><td>Magic: Plant Spells</td></tr>
+					<tr><td><b>MA</b></td><td>Martial Arts</td></tr>
+					<tr><td><b>MAFCCS</b></td><td>Martial Arts: Fairbairn Close Combat Systems</td></tr>
+					<tr><td><b>MH1:</b></td><td>Monster Hunters 1: Champions</td></tr>
+					<tr><td><b>MYST</b></td><td>Mysteries</td></tr>
+					<tr><td><b>MYTH</b></td><td>Myth (3E)</td></tr>
+					<tr><td><b>P</b></td><td>Powers</td></tr>
+					<tr><td><b>PSI</b></td><td>Psionic Powers</td></tr>
+					<tr><td><b>PU1:</b></td><td>Power-Ups 1: Imbuements</td></tr>
+					<tr><td><b>PU2:</b></td><td>Power-Ups 2: Perks</td></tr>
+					<tr><td><b>PU3:</b></td><td>Power-Ups 3: Talents</td></tr>
+					<tr><td><b>PY#:</b></td><td>Pyramid issues (replace # with the issue number, but leave out the leading "3-")</td></tr>
+					<tr><td><b>RSWL</b></td><td>Reign of Steel: Will to Live</td></tr>
+					<tr><td><b>SU</b></td><td>Supers</td></tr>
+					<tr><td><b>TMS</b></td><td>Thaumatology - Magical Styles</td></tr>
+					<tr><td><b>UT</b></td><td>Ultra-Tech</td></tr>
+					<tr><td><b>VOR</b></td><td>Vorkosigan Saga</td></tr>
+				</table></p>
+			</div>
+		</div>
+		<?php include 'parts/footer.php'; ?>
+	</body>
+</html>
