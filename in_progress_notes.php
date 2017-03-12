056611b90e389304bfa238b223b428d6cc3d2c1f
Richard Wilkes <wilkes@me.com>
2017-03-12 13:23:07 -0700

    Update for recent changes

--- in_progress_notes.php
+++ in_progress_notes.php
@@ -15,15 +15,19 @@
 					<h3>Features Added</h3>
 					<ul>
 						<li>Partial implementation of <a href="https://gurpscharactersheet.atlassian.net/browse/GCS-25">GCS-25</a>: Skills &amp; spells can now be based off of 10 rather than a stat.</li>
+						<li><a href="https://gurpscharactersheet.atlassian.net/browse/GCS-151">GCS-151</a>: Add self-control roll option of N/A.</li>
 					</ul>
 					<h3>Bugs Fixed</h3>
 					<ul>
 						<li>Unidentified keys will now have the key name emitted as part of the error message when exporting a text template. Previously, most cases would emit a "%s" instead.</li>
+						<li><a href="https://gurpscharactersheet.atlassian.net/browse/GCS-143">GCS-143</a>: The 'Use GURPS Metric Rules' option isn't honored correctly when calculating the total weight of container.</li>
+						<li><a href="https://gurpscharactersheet.atlassian.net/browse/GCS-152">GCS-152</a>: Size modifier incorrectly affecting ST pricing with KYOS rules enabled.</li>
 					</ul>
 					<h3>Data File Changes</h3>
 					<ul>
 						<li>Added the Gray Necromancer from Pyramid 3-50.</li>
 						<li>Added spells from Pyramid 3-50.</li>
+						<li><a href="https://gurpscharactersheet.atlassian.net/browse/GCS-149">GCS-149</a>: Skill Dancing should default to DX-5.</li>
 					</ul>
 				</div>
 			</div>
